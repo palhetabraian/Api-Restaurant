6d140c25fc8eb93cf6dad51c7490ee0e3adb7989
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-06-09 10:12:09 -0300

    Criando a Migration de Produtos.

--- src/database/migrations/20260609125826_create-products.ts
+++ src/database/migrations/20260609125826_create-products.ts
@@ -1,10 +1,16 @@
-import type { Knex } from "knex";
+import type { Knex } from 'knex';
 
-
+// Criando a migratino para criar a tabela products
 export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTable('products', (table) => {
+        table.increments('id').primary();
+        table.text('name').notNullable();
+        table.decimal('price').notNullable();
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('updated_at').defaultTo(knex.fn.now());
+    });
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTable('products');
 }
-
