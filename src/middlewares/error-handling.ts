ba0a540752220ad0e9ad625e09ca105b087e3897
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-06-09 20:45:26 -0300

    Adicionando e configurando Zod

--- src/middlewares/error-handling.ts
+++ src/middlewares/error-handling.ts
@@ -1,9 +1,16 @@
 import { Request, Response, NextFunction } from 'express';
 import { AppError } from '@/utils/AppError';
+import { ZodError } from 'zod';
 
 export function errorHandling(error: any, request: Request, response: Response, _: NextFunction) {
+    // tratamento quando é uma classe instanciado pelo app erro quando o erro vem da aplicacao
     if (error instanceof AppError) {
         return response.status(error.statusCode).json({ message: error.message });
+    }
+
+    //Criando tratacao quando é a partir dos dados enviados e verificando com o zod
+    if (error instanceof ZodError) {
+        return response.status(400).json({ message: 'validation error', issues: error.format() });
     }
 
     return response.status(500).json({ message: error.message });
