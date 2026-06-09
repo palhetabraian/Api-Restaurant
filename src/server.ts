e34335d30d161b81fc6ab66588731102783e6a76
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-06-09 09:20:00 -0300

    Criando o Middleware de Tratamento de Exceções

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import { routes } from './routes';
+import { errorHandling } from './middlewares/error-handling';
 
 const PORT = 3333;
 
@@ -8,7 +9,9 @@ const app = express();
 
 app.use(express.json());
 
-// Chamando as rotas
+// Chamando todas as rotas da pagina routes
 app.use(routes);
+
+app.use(errorHandling);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
---
+++ src/utils/AppError.ts
@@ -0,0 +1,13 @@
+// Arquivo responsavel para tratamento de erros
+
+class AppError {
+    message: string;
+    statusCode: number;
+
+    constructor(message: string, statusCode: number = 400) {
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+}
+
+export { AppError };
