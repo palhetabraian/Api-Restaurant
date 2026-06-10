f6e52bf8ed9454a16ee627751dfa5cd53dc9db05
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-06-09 21:17:25 -0300

    Validando dados do produto

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,11 +1,32 @@
 import { NextFunction, Request, Response } from 'express';
-import { AppError } from '@/utils/AppError';
+import { z } from 'zod';
 
 // Metodos do nosso controller
 class ProductsController {
+    // Controler responsavel por listar os produtos
     async index(request: Request, response: Response, next: NextFunction) {
         try {
             return response.json({ message: 'ok' });
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    //controller responsavel por adicionar produto
+    async create(request: Request, response: Response, next: NextFunction) {
+        try {
+            // Fazendo a validacao com zod
+            const bodySchema = z.object({
+                // trim serve para tirar os espacos e min para no minimo 6 digitos
+                name: z.string().trim().min(6),
+                // gt pelo menos maior que 0 o preco do produto
+                price: z.number().gt(0, { message: 'value must be greater than 0' }),
+            });
+
+            const { name, price } = bodySchema.parse(request.body);
+
+            // status 201 criado com sucesso.
+            return response.status(201).json({ name, price });
         } catch (error) {
             next(error);
         }
--- src/routes/products-routes.ts
+++ src/routes/products-routes.ts
@@ -4,6 +4,9 @@ import { Router } from 'express';
 const productsRoutes = Router();
 const productsController = new ProductsController();
 
+// Rota de lista produtos
 productsRoutes.get('/', productsController.index);
+//Rota responsavel por criar produtos
+productsRoutes.post('/', productsController.create);
 
 export { productsRoutes };
