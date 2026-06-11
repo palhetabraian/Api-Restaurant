0379b159170a62b9fdc99641a1fe2662df860345
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-06-11 00:31:43 -0300

    Fechando A Mesa.

--- src/controllers/tables-sessions-controller.ts
+++ src/controllers/tables-sessions-controller.ts
@@ -64,6 +64,19 @@ class TablesSessionsController {
                 .refine((value) => !isNaN(value), { message: 'id must be a number' })
                 .parse(request.params.id);
 
+            //Fechando a Mesa
+            const session = await knex<TablesSessionsRepository>('tables_sessions').where({ id }).first();
+
+            if (!session) {
+                throw new AppError('session table not found');
+            }
+
+            if (session.closed_at) {
+                throw new AppError('this session table is already closed');
+            }
+
+            await knex<TablesSessionsRepository>('tables_sessions').update({ closed_at: knex.fn.now() }).where({ id });
+
             return response.json();
         } catch (error) {
             next(error);
