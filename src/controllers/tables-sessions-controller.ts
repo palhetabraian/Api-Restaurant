2e8bb650f9c73039403957c13c3f8d56b8296619
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-06-10 23:37:05 -0300

    Refinando abertura da mesa

--- src/controllers/tables-sessions-controller.ts
+++ src/controllers/tables-sessions-controller.ts
@@ -1,6 +1,7 @@
 // Responsavel por fazer as funcionalidades da rota (regra de negocio)
 // controller responsavel por funcionalidades da rota
 import { Request, Response, NextFunction } from 'express';
+import { AppError } from '@/utils/AppError';
 //importando zod para validar os dados passado na requisicao
 import { knex } from '@/database/knex';
 import { z } from 'zod';
@@ -14,6 +15,19 @@ class TablesSessionsController {
 
             // validando os dados passado pelo zod
             const { table_id } = bodySchema.parse(request.body);
+
+            //Fazendo verificacao para nao deixar abrir uma mesa que ja esta aberta
+            const session = await knex<TablesSessionsRepository>('tables_sessions')
+                .where({
+                    table_id,
+                })
+                .orderBy('opened_at', 'desc')
+                .first();
+
+            // Verificando se existe a mesa aberta
+            if (session && !session.closed_at) {
+                throw new AppError('this table is already open');
+            }
 
             // inserindo dados na tabela de tables_sessions
             await knex<TablesSessionsRepository>('tables_sessions').insert({
