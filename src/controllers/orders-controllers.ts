3ce2fa308a12721d3683503603c93237de8c4347
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-06-11 23:28:19 -0300

    Salvando Pedido.

--- src/controllers/orders-controllers.ts
+++ src/controllers/orders-controllers.ts
@@ -29,14 +29,22 @@ class OrdersController {
             }
 
             // verificando se o produto existe
-            const product = await knex<TablesSessionsRepository>('products').where({ id: product_id }).first();
+            const product = await knex<ProductRepository>('products').where({ id: product_id }).first();
 
             // verificando se o produto nao existe
             if (!product) {
                 throw new AppError('product not found');
             }
 
-            return response.status(201).json(product);
+            // salvando o pedido na tabela
+            await knex<OrderRepository>('orders').insert({
+                table_session_id,
+                product_id,
+                quantity,
+                price: product.price,
+            });
+
+            return response.status(201).json();
         } catch (error) {
             next(error);
         }
